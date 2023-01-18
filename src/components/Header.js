bfa173cf4fdc9379ad57d429788a830c3bb22ccd
Anirudh Chintha <anirudhchintha95@gmail.com>
2023-01-18 16:27:17 -0500

    Make header clickable and add scrollToTop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 const Header = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <header className="h-12 bg-p-1 w-full fixed top-0 z-[999]">
       <div className="h-full flex text-center items-center justify-center">
-        <div className="box-border bg-slate-100 p-1 text-p-1 font-semibold rounded-sm">
+        <div
+          className="box-border bg-slate-100 p-1 text-p-1 font-semibold rounded-sm cursor-pointer"
+          onClick={scrollToTop}
+        >
           Hanlon Labs Events
         </div>
       </div>
